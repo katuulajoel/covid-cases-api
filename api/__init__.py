26ea26945a786ad8f5da6d13e88c46a1d9d42d96
Katuula <katuula@gmail.com>
2022-05-09 11:08:44 +0300

    remove unused apps

--- api/__init__.py
+++ api/__init__.py
@@ -2,9 +2,6 @@ import os
 
 from flask import Flask
 
-from blueprints.basic_endpoints import blueprint as basic_endpoint
-from blueprints.jinja_endpoint import blueprint as jinja_template_blueprint
-from blueprints.documented_endpoints import blueprint as documented_endpoint
 from api.v1 import blueprint as covid_api_v1
 from config import app_config
 from api.db import db, migrate
@@ -18,7 +15,4 @@ db.init_app(app)
 migrate.init_app(app, db)
 ma.init_app(app)
 
-app.register_blueprint(basic_endpoint)
-app.register_blueprint(jinja_template_blueprint)
-app.register_blueprint(documented_endpoint)
 app.register_blueprint(covid_api_v1)
--- manage.py
+++ manage.py
@@ -1,3 +1,5 @@
+import requests
+import json
 from flask_script import Manager
 from flask_migrate import MigrateCommand
 
@@ -9,8 +11,36 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def hello():
-    r = requests.get('http://www.google.com')
-    print('test')
+    r = requests.get('https://covid-api.mmediagroup.fr/v1/cases')
+    f = open("cases.json", "x")
+    f.write(r.text)
+    f.close()
+
+@manager.command
+def seed_database():
+    with open('cases.json') as cases_file:
+        data = json.load(cases_file)
+
+    my_list = []
+    for key in data:
+        country_case = data[key]['All']
+        dict = {
+            'country': country_case.get('country'),
+            'city': country_case.get('city'),
+            'continent': country_case.get('continent'),
+            'confirmed': country_case.get('confirmed'),
+            'deaths': country_case.get('deaths'),
+            'population': country_case.get('population'),
+            'life_expectancy': country_case.get('life_expectancy'),
+            'updated_at': country_case.get('updated')
+        }
+        my_list.append(dict)
+
+    # Serializing json 
+    json_object = json.dumps({"model": "models.CaseModel", "data": my_list}, indent = 4)
+
+    with open("cases_seed.json", "w") as outfile:
+        outfile.write(json_object)
 
 if __name__ == '__main__':
     manager.run()

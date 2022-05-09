1c0c2708bc42f6ddc8c10a46fbc78239557f5f2e
Katuula <katuula@gmail.com>
2022-05-09 10:07:41 +0300

    fix requirements versions

--- api/__init__.py
+++ api/__init__.py
@@ -10,22 +10,15 @@ from config import app_config
 from api.db import db, migrate
 from api.ma import ma
 
-def create_app(config_name):
-    app = Flask(__name__)
-    flask_config = os.environ.get('FLASK_ENV')
-    app.config.from_object(app_config[flask_config])
+app = Flask(__name__)
+flask_config = os.environ.get('FLASK_ENV')
+app.config.from_object(app_config[flask_config])
 
-    db.init_app(app)
-    migrate.init_app(app, db)
-    ma.init_app(app)
+db.init_app(app)
+migrate.init_app(app, db)
+ma.init_app(app)
 
-    app.register_blueprint(basic_endpoint)
-    app.register_blueprint(jinja_template_blueprint)
-    app.register_blueprint(documented_endpoint)
-    app.register_blueprint(covid_api_v1)
-
-    return app
-
-if __name__ == "__main__":
-    create_app.run(debug=True)
-
+app.register_blueprint(basic_endpoint)
+app.register_blueprint(jinja_template_blueprint)
+app.register_blueprint(documented_endpoint)
+app.register_blueprint(covid_api_v1)
--- manage.py
+++ manage.py
@@ -1,15 +1,16 @@
 from flask_script import Manager
 from flask_migrate import MigrateCommand
 
-from api import create_app
+from api import app
 from api.v1.models import *
 
-manager = Manager(create_app)
+manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 @manager.command
 def hello():
-     print('test')
+    r = requests.get('http://www.google.com')
+    print('test')
 
 if __name__ == '__main__':
     manager.run()

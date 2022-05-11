6d305028abf08362ca07bddf90a88b278aba6a1f
Katuula <katuula@gmail.com>
2022-05-11 09:03:17 +0300

    tweaked up for heroku

--- config.py
+++ config.py
@@ -13,7 +13,8 @@ class Config:
     DEBUG = False
     CSRF_ENABLED = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgres://xcnhueiaxsostk:1a007886680381b935a35cab5a350005143fd846a42cd89a198b6404a438d673@ec2-18-210-64-223.compute-1.amazonaws.com:5432/d5bhi9en1h3oe1'
 
 
 class DevelopmentConfig(Config):
